95d5c26ccaae8e7ef31ddff18d373fda9a2d65b1
Nir Tamir <nirtamir2@gmail.com>
2018-12-04 23:44:16 +0200

    added prettier + format

--- keys.js
+++ keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoURI: 'mongodb://vulgramming:data123@ds055990.mlab.com:55990/fitness-app-db'
-}
+  mongoURI:
+    "mongodb://vulgramming:data123@ds055990.mlab.com:55990/fitness-app-db"
+};
--- server.js
+++ server.js
@@ -1,15 +1,20 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 
 const app = express();
 
 app.use(bodyParser.json());
 
-const db = require('./keys').mongoURI;
+const db = require("./keys").mongoURI;
+
 mongoose
-    .connect(db, {useNewUrlParser: true})
-    .then(() => console.log('Connected'))
-    .catch(err => console.log(err));
+  .connect(
+    db,
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log("Connected"))
+  .catch(err => console.log(err));
+
 const port = process.env.PORT || 5000;
-app.listen(port, () => console.log('Server running on port ' + port));
+app.listen(port, () => console.log("Server running on port " + port));
