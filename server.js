9753b9f7d3970796134b6b125146d55cb54af2a9
Kuba <kubaz9991@gmail.com>
2018-12-04 21:34:21 +0100

    fixed console.log 'server running...'

--- server.js
+++ server.js
@@ -12,4 +12,4 @@ mongoose
     .then(() => console.log('Connected'))
     .catch(err => console.log(err));
 const port = process.env.PORT || 5000;
-app.listen(port, () => console.log('Server running on port $(port)'));
+app.listen(port, () => console.log('Server running on port ' + port));
