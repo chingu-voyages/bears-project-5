dcb6e757846ae5c0176f9ed068900fce8bd6382e
Nir Tamir <nirtamir2@gmail.com>
2018-12-05 17:46:19 +0200

    fix eslint warnings

--- server/server.js
+++ server/server.js
@@ -11,5 +11,6 @@ mongoose
     .connect(db, {useNewUrlParser: true})
     .then(() => console.log('Connected'))
     .catch(err => console.log(err));
+    
 const port = process.env.PORT || 5000;
-app.listen(port, () => console.log('Server running on port $(port)'));
+app.listen(port, () => console.log(`Server running on port ${port}`));
