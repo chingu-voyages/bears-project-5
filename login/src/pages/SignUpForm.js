0aad749d89010bc2c5672300f2f15b1143e4dafb
vulgramming <vulgramming@gmail.com>
2018-12-05 01:19:54 +0100

    Login Design (Work in progress)

---
+++ login/src/pages/SignUpForm.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+class SignUpForm extends Component {
+    render() {
+        return (
+            <div className="FormCenter">
+                <form className="FormFields" onSubmit="{this.handleSubmit}">
+                    <div className="FormField">
+                        <label className="FormField__Label" htmlFor="username">Username</label>
+                        <input type="text" id="username" className="FormField__Input" placeholder="Enter your username" name="username"></input>
+                    </div>
+                    <div className="FormField">
+                        <label className="FormField__Label" htmlFor="email">Email</label>
+                        <input type="email" id="email" className="FormField__Input" placeholder="Enter your email" name="email"></input>
+                    </div>
+                    <div className="FormField">
+                        <label className="FormField__Label" htmlFor="password">Password</label>
+                        <input type="password" id="password" className="FormField__Input" placeholder="Enter your password" name="password"></input>
+                    </div>
+                    <div className="FormField">
+                        <label className="FormField__CheckboxLabel">
+                            <input className="FormField__Checkbox" type="checkbox" name="hasAgreed" /> I agree to all statments in <a href="" className="FormField__TermsLink">terms of service</a>
+                        </label>
+                    </div>
+                    <div className="FormField">
+                        <button className="FormField__Button mr-20">Sign Up</button>
+                        <Link to="/sign-in" className="FormField__Link">I am already a member</Link>
+                    </div>
+                </form>
+            </div>
+        );
+    }
+}
+
+export default SignUpForm;
