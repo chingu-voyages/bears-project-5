c1f240a126ee6ab6a7c4428e9afe05385b637544
Kuba <kubaz9991@gmail.com>
2018-12-09 13:05:31 +0100

    graphql setup

---
+++ server/schema.js
@@ -0,0 +1,19 @@
+const {
+    GraphQLObjectType,
+    GraphQLString,
+    GraphQLInt,
+    GraphQLSchema,
+    GraphQLList,
+    GraphQLNonNull
+} = require('graphql');
+
+
+const RootQuery = new GraphQLObjectType({
+
+});
+
+
+
+module.exports = new GraphQLSchema({
+
+});
--- server/server.js
+++ server/server.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const items = require("./routes/api/items");
+const expressGraphQL = require('express-graphql');
+const schema = require("./schema.js");
 
 const app = express();
 
@@ -16,6 +19,17 @@ mongoose
   )
   .then(() => console.log("Connected"))
   .catch(err => console.log(err));
+//use REST routes
+// app.use('/api/items', items);
+
+//use GraphQL
+
+app.use('/graphql', expressGraphQL({
+  schema: schema,
+  graphiql: true
+}));
+
+
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log("Server running on port " + port));
