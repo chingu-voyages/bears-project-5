0634fed3fdef88fbcf7effe544cbf98c27f3eeac
Kuba <kubaz9991@gmail.com>
2018-12-10 21:01:17 +0100

    graphql working

--- server/schema.js
+++ server/schema.js
@@ -1,3 +1,4 @@
+const _ = require('lodash');
 const {
     GraphQLObjectType,
     GraphQLString,
@@ -7,19 +8,42 @@ const {
     GraphQLNonNull
 } = require('graphql');
 
+var foods = [
+    {
+        name: "banana", 
+        kcal: 150,
+        id: "1"
+    },
+    {
+        name: "chicken",
+        kcal: 300,
+        id: "2"
+    }
+]
 
-const FoodQuery = new GraphQLObjectType({
+const FoodType = new GraphQLObjectType({
     name: 'Food',
     fields: ()=>({
         id: {type: GraphQLString},
         name: {type: GraphQLString},
         kcal: {type: GraphQLInt}
-
-    });
+    })
 });
 
-
+const RootQuery = new GraphQLObjectType({
+    name:'RootQueryType',
+    fields: {
+        food: {
+            type: FoodType,
+            args: {id: {type: GraphQLString}},
+            resolve(parent, args){
+                //code to get data from db
+                return _.find(foods, {id:args.id});
+            }
+        }
+    }
+});
 
 module.exports = new GraphQLSchema({
-
+    query: RootQuery
 });
--- server/server.js
+++ server/server.js
@@ -2,24 +2,25 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const expressGraphQL = require('express-graphql');
-// const schema = require("./schema.js");
+const schema = require("./schema.js");
 
 const app = express();
 const db = require("./keys").mongoURI;
 app.use(bodyParser.json());
 
 app.use('/graphql', expressGraphQL({
-  
+  schema,
+  graphiql: true
 }));
 
 /* eslint-disable no-console */
-mongoose
-  .connect(
-    db,
-    { useNewUrlParser: true }
-  )
-  .then(() => console.log("Connected"))
-  .catch(err => console.log(err));
+// mongoose
+//   .connect(
+//     db,
+//     { useNewUrlParser: true }
+//   )
+//   .then(() => console.log("Connected"))
+//   .catch(err => console.log(err));
 
 
 
