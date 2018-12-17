a845112d8ca635b2b35ab95257de4877231beed8
Kuba <kubaz9991@gmail.com>
2018-12-17 20:41:37 +0100

    react router nav

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,27 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link, NavLink } from 'react-router-dom';
 import './App.css';
 import SignUpForm from './pages/SignUpForm.js';
 import SignInForm from './pages/SignInForm.js'
+import LandingPage from './pages/LandingPage';
 
 class App extends Component {
   render() {
     return (
       <Router>
-        <div className="App">
-          <div className="App__Aside"></div>
-          <div className="App__Form">
-
+        <div className="container">
             <div className="PageSwitcher">
-              <NavLink href="#" to="/sign-in" activeClassName="PageSwitcher__Item--Active" className="PageSwitcher__Item">Sign In</NavLink>
-              <NavLink href="#" exact to="/" activeClassName="PageSwitcher__Item--Active" className="PageSwitcher__Item">Sign Up</NavLink>
-            </div>
+              <NavLink href="#" to="/" activeClassName="PageSwitcher__Item--Active" className="PageSwitcher__Item">Home</NavLink>
+              <NavLink href="#" to="/signin" activeClassName="PageSwitcher__Item--Active" className="PageSwitcher__Item">Sign In</NavLink>
+              <NavLink href="#" exact to="/signup" activeClassName="PageSwitcher__Item--Active" className="PageSwitcher__Item">Sign Up</NavLink>
+            </div>        
+        <hr />
+        <h1> Fitness App</h1>
 
-            <div className="FormTitle">
-              <NavLink href="#" to="/sign-in" activeClassName="FormTitle__Link--Active" className="FormTitle__Link">Sign In</NavLink> or 
-              <NavLink href="#" exact to="/" activeClassName="FormTitle__Link--Active" className="FormTitle__Link ">Sign Up</NavLink>
-            </div>
-            <Route exact path="/" component={SignUpForm}>
-            </Route>
-            <Route path="/sign-in" component={SignInForm}>
-            </Route>
-          </div>
+          <Route  path="/signup" component={SignUpForm}/>
+          <Route path="/signin" component={SignInForm} />
+          <Route exact path="/" component={LandingPage} />
+        
         </div>
       </Router>
     );
