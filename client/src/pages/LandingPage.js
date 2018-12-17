8b81fe68ff38c1f9d0ecd73db142c47abccd577e
Kuba <kubaz9991@gmail.com>
2018-12-17 20:41:14 +0100

    added landing page

---
+++ client/src/pages/LandingPage.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+
+class LandingPage extends Component {
+  render() {
+    return (
+      <div>
+          <h1>landing page</h1>
+      </div>
+    );
+  }
+}
+
+export default LandingPage;
