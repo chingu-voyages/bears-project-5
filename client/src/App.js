2d72b6a0aa7b8eede975a075b0623aad43a43f0d
Nir Tamir <nirtamir2@gmail.com>
2019-01-05 14:30:50 +0200

    client food list item

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import ApolloClient from 'apollo-boost'
 import { ApolloProvider } from 'react-apollo'
 import SignUpForm from './components/SignUpForm.js'
--- client/src/components/FoodList.js
+++ client/src/components/FoodList.js
@@ -1,35 +1,33 @@
-import React, { Component } from 'react';
-import { graphql } from 'react-apollo';
-import {getFoodQuery} from '../queries/queries';
+import React, { Component } from 'react'
+import { graphql } from 'react-apollo'
+import { getFoodQuery } from '../queries/queries'
+import { FoodListItem } from './FoodListItem'
+import './FoodList.css'
 
 class FoodList extends Component {
-
-    displayFood(){
-        console.log(this.props);
-        var data = this.props.data;
-        console.log(this.props);
-            if(data.loading){
-                return (<div>Food loading...</div>)
-            } else{
-                return data.food.map(food=>{
-                    return (<li key={food.id}>{food.name}, {`kcal: ${food.kcal}`}</li>)
-                });
-            }
-    }
-
-
-    render(){
-
-        return(
-            
-            <div>
-                <ul>
-                    {this.displayFood()}
-                </ul>
-            </div>
-        )
-    }
-
+  render() {
+    const { data } = this.props
+    console.log(data)
+    return data.loading ? (
+      <div>Food loading...</div>
+    ) : (
+      <div>
+        {data.food.map(food => {
+          return (
+            <ul className="FoodList">
+              <FoodListItem
+                key={food.id}
+                title={food.name}
+                subTitle="a"
+                caloryCount={food.kcal}
+                image="https://i5.walmartimages.ca/images/Large/580/6_r/875806_R.jpg"
+              />
+            </ul>
+          )
+        })}
+      </div>
+    )
+  }
 }
 
-export default graphql(getFoodQuery)(FoodList);
+export default graphql(getFoodQuery)(FoodList)
