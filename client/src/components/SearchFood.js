1e880aaaa0e886fa574a1b681481c183f238ba89
Kuba <kubaz9991@gmail.com>
2019-01-16 19:17:23 +0100

    searchfood not working

--- client/src/components/SearchFood.js
+++ client/src/components/SearchFood.js
@@ -1,42 +1,83 @@
 import React, { Component } from 'react';
-import { graphql} from 'react-apollo';
-import {searchFoodQuery} from '../queries/queries';
-
+import { Query} from 'react-apollo';
+import  { searchFoodQuery } from '../queries/queries';
 
 class SearchFood extends Component {
-    constructor(props){
-        super(props);
-            this.state = {
-                searchText: ""
+    
+            state = {
+                searchText: "banana"
             }
-    }
-
+    
     executeSearch(e){
         e.preventDefault();
-        console.log(this.props)
-            
-    }
+      }
     
-  render() {
-    
-   console.log(this.state.searchText);
+  render() {   
+      
+    return (        
+        <Query query={searchFoodQuery} variables={{ name: this.state.searchText}}>
+                {({ loading, error, data }) => {
+      if (loading) return null;
+      if (error) return `Error!: ${error}`;
+      
+      console.log(data);   
 
-    return (
-      <div className="Search">
+      return (
+        <div>
+            {data.food.map((item)=>{
+               return  <p>{item}</p>
+            })}
+            <h1>hello</h1>
+                             
 
-        <h1>hello</h1>
-        <form onSubmit={this.executeSearch.bind(this)}>
-            <input type="text" onChange={e=>{
-                this.setState({
-                    searchText: e.target.value
-                })
-            }}></input>
-            <button>Search!</button>
-        </form>
-
-      </div>
+            <form onSubmit={this.executeSearch.bind(this)}>
+                <input type="text" onChange={e=>{
+                    this.setState({
+                        searchText: e.target.value
+                    })
+                }}></input>
+                <button>Search!</button>
+            </form>
+        </div>
+      );
+    }}
+      
+    </Query>  
     )
   }
 }
 
-export default graphql(searchFoodQuery)(SearchFood)
+export default SearchFood;
+
+
+
+
+
+
+
+//export class SearchFood extends Component() {
+
+    //     render(){
+    //     return (
+    //       <Query query={searchFoodQuery} variables={{ name: "banana"}}>
+    //         {({ data, loading, error }) => {
+    //           if (loading) return <p>Loading</p>
+    //           if (error) return <p>ERROR</p>;
+    //           return (
+    //             <div>
+    //             <h1>hello</h1>
+    //             <form onSubmit={this.executeSearch.bind(this)}>
+    //                 <input type="text" onChange={e=>{
+    //                     this.setState({
+    //                         searchText: e.target.value
+    //                     })
+    //                 }}></input>
+    //                 <button>Search!</button>
+    //             </form>
+    //             </div>
+    //           );
+    //         }}
+    //       </Query>
+    //     );
+    //     }
+    //   };
